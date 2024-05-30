49464f2430c480822217f31b83e38370aac88f20
Therealirfanshafi <thatirfanshafi@gmail.com>
2024-05-30 22:30:32 +0600

    Fixed issue with images

--- actual_code/quiz_app/quiz_app/urls.py
+++ actual_code/quiz_app/quiz_app/urls.py
@@ -17,10 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('main.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # adapted from https://stackoverflow.com/questions/36280056/page-not-found-404-django-media-files
